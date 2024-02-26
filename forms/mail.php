e70000c375cf6028038b2411e3f094698774b890
MihajloIT <mihajlo.stefanovic011@gmail.com>
2024-02-26 21:43:41 +0100

    mailer 1

---
+++ forms/mail.php
@@ -0,0 +1,19 @@
+<?php
+//get data from form  
+
+$name = $_POST['name'];
+$email= $_POST['email'];
+$subject = $_POST['subject'];
+$message= $_POST['message'];
+
+$to = "mihajlo1618@gmail.com";
+
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+$headers = "From: noreply@izradavebsajtova.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:thankyou.html");
+?>
